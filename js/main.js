78b97831582706ac534388f2af52341b030ca7da
joanManuel <joan.torres.jt@gmail.com>
2017-09-04 22:44:50 -0500

    Terminando de agregar los plugin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function(){
+    //Lettering
+    $('.nombre-sitio').lettering();
+    
+    
     //programa de Conferencia
     $('.programa-evento .info-curso:first').show();
     $('.menu-programa a:first').addClass('activo');
@@ -15,6 +19,24 @@ $(function(){
     return false;
  });
 });
+
+//Animaciones para los números
+$('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6},1200);
+$('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15},1200);
+$('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3},1500);
+$('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9},1500);
+
+//Cuenta regresiva
+
+$('.cuenta-regresiva').countdown('2017/10/07 09:00:00', function(event){
+    $('#dias').html(event.strftime('%D'));
+    $('#horas').html(event.strftime('%H'));
+    $('#minutos').html(event.strftime('%M'));
+    $('#segundos').html(event.strftime('%S'));
+});
+
+
+
 
 //API DE GOOGLE MAPS
 
